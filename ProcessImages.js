f9605e6f3e66b638912b86846ef6f7372121b75e
xenotime-india <sandeepkhoj@gmail.com>
2017-08-09 22:27:51 +0530

    added lint

--- ProcessImages.js
+++ ProcessImages.js
@@ -14,8 +14,8 @@ class ProcessImages {
     this.sftpConfig = {
       host: config.SFTP_HOST,
       username: config.SFTP_USER_NAME,
-      password: config.SFTP_PASSWORD
-    }
+      password: config.SFTP_PASSWORD,
+    };
 
     this.s3Client = s3.createClient({
       maxAsyncS3: 20,     // this is the default
@@ -25,9 +25,14 @@ class ProcessImages {
       multipartUploadSize: 15728640, // this is the default (15 MB)
       s3Options: {
         accessKeyId: config.AWS_S3_ACCESS_KEY_ID,
-        secretAccessKey: config.AWS_S3_SECRET_ACCESS_KEY
-      }
+        secretAccessKey: config.AWS_S3_SECRET_ACCESS_KEY,
+      },
     });
+
+    this.s3Params = {
+      Bucket: config.AWS_S3_BUCKET_NAME,
+      Prefix: config.AWS_S3_FOLDER_PREFIX,
+    };
 
     this.fsoptions = {
       overwrite: true,
@@ -35,13 +40,12 @@ class ProcessImages {
       dot: true,
       junk: true,
       filter: [
-        /(\.|\/)(tif|jpe?g|png)$/i
+        /(\.|\/)(tif|jpe?g|png)$/i,
       ],
       rename: function(filePath) {
         return filePath.replace(/^.*[\\\/]/, '');
-      }
+      },
     };
-
   }
 
   start = async () => {
@@ -50,35 +54,31 @@ class ProcessImages {
       const session = await sftp.session(this.sftpConfig);
 
       const fileName = dateFormat(new Date(), 'mmddyyyy');
-      const status = await sftp.getStream('~/Dev/711_idpi_images.07_14_2017_03_35_24.tar', tar.extract(path.join(__dirname, 'tmp', fileName + '_tar')), session)
+      const status = await sftp.getStream('~/Dev/711_idpi_images.07_14_2017_03_35_24.tar', tar.extract(path.join(__dirname, 'tmp', fileName + '_tar')), session);
 
-      winston.log("Downloaded and Unziped file")
+      winston.log('Downloaded and Unziped file');
 
       if (status) {
-        await copy(path.join(__dirname, 'tmp', fileName + '_tar'), path.join(__dirname, 'tmp', fileName + '_new'), this.fsoptions)
+        await copy(path.join(__dirname, 'tmp', fileName + '_tar'), path.join(__dirname, 'tmp', fileName + '_new'), this.fsoptions);
 
         const params = {
           localDir: path.join(__dirname, 'tmp', fileName + '_new'),
           deleteRemoved: true, // default false, whether to remove s3 objects
                                // that have no corresponding local file.
-          s3Params: {
-            Bucket: config.AWS_S3_BUCKET_NAME,
-            Prefix: config.AWS_S3_FOLDER_PREFIX,
-          },
+            s3Params: this.s3Params,
         };
-        this.s3Client.uploadDir(params).on('error', function (err) {
-          winston.error("unable to sync:", err.stack);
-        }).on('progress', function () {
-          winston.log("progress..");
-        }).on('end', function () {
-          winston.log("done uploading");
+        this.s3Client.uploadDir(params).on('error', function(err) {
+          winston.error('unable to sync:', err.stack);
+        }).on('progress', function() {
+          winston.log('progress..');
+        }).on('end', function() {
+          winston.log('done uploading');
         });
       }
-    }
-    catch(ex) {
+    } catch(ex) {
       winston.error(ex);
     }
-  }
+  };
 }
 
 export default ProcessImages;
